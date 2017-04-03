c40c06e60baba9417354aa800badea7fcdefa6b9
Barasbi <betokov93@gmail.com>
2017-04-03 16:28:20 +0300

    add

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
-﻿<?php require_once('includes/header.php'); ?>
-	
+<?php require_once('includes/header.php'); ?>
+<?php require_once('includes/header-top.php'); ?>
+<?php require_once('includes/header_bottom.php'); ?>
+<?php require_once('includes/sale.php'); ?>
 <?php require_once('includes/footer.php'); ?>
-
-	
+<!-- JS_BLOCK -->
+<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+<script src="libs/owl.carousel/owl.carousel.js"></script>
+<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
+<script src="js/responsiveTabs.js"></script>
+<script src="js/jquery.maskedinput.min.js"></script>
+<script src="js/common.js"></script>
 </body>
 </html>
-
