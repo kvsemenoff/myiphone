a87c5db4089a8f5941c43825733b9806164713c7
Barasbi <betokov93@gmail.com>
2017-04-04 13:57:43 +0300

    add

--- includes/header-top.php
+++ includes/header-top.php
@@ -19,3 +19,22 @@
 		</div>
 	</div>
 </section>
+		<!-- MASK -->
+		<div class="mask" id="js-mask"></div>
+		<!-- POPAPS -->
+		<div class="js-window" id="js-form1">
+			<form action="#" method="post" class="js-submit db-form1">
+				<p>Закажите<br>
+					обратный звонок!</p>
+					<input type="text" name="uname" placeholder="Ваше имя" class="input_view input_view__name db-input1"><br><br>
+					<input type="text" name="phone" placeholder="Ваш телефон" class="js-phone input_view  db-input2 input_view__phone"><br><br>
+					<input type="submit" name="submit" value="заказать" class="button_view db-submit">
+				</form>
+			</div>
+			<div class="js-window" id="js-form2">
+				<p class="thanks">Спасибо за заявку! <br>
+					Наш менджер свяжется с Вами.</p>
+
+				</div>
+				<a href="#js-form2" name="js-modal"></a>
+	<!-- 			<a href="#js-form1" name="js-modal">popap</a> -->
--- js/common.js
+++ js/common.js
@@ -1,6 +1,71 @@
 
 
 $(document).ready(function(){
+  $('.js-phone').mask("+7 (999) 999 - 99 - 99?");
+
+   $('a[name="js-modal"]').on("click", function(e){
+        e.preventDefault();
+        var  id = $(this).attr('href'),
+        winW = $(window).width(),
+        winH = $(window).height();
+        $(id).css("left", winW/2-$(id).width()/2);
+        $(id).css("top", winH/2-$(id).height()/2);
+        $('body').css("overflow-y", "hidden");
+        $(id).fadeIn();
+        $('#js-mask').fadeIn();
+    });
+    
+
+    $('#js-mask').on("click", function(){
+        $('#js-mask').hide();
+        $('.js-window').hide();
+        $('body').removeAttr("style");
+
+    });
+
+            $(function() {
+                $(window).scroll(function() {
+                    if($(this).scrollTop() != 0) {
+                        $('#toTop').fadeIn();
+                    } else {
+                        $('#toTop').fadeOut();
+                    }
+                });
+                $('#toTop').click(function() {
+                    $('body,html').animate({scrollTop:0},800);
+                });
+            });
+
+    /*MODAL SUBMIT*/
+    $('.js-submit').submit(function(){
+        var phone = $(this).find('input[name="phone"]');
+        
+        if(phone.val() == ""){
+            phone.focus();
+            return false;
+        }
+
+        else{
+            var form_data = $(this).serialize(); 
+            $.ajax({
+                type: "POST", 
+                url: "/message.php", 
+                data: form_data,
+                success: function() {
+                    cleanTnanks(this);
+                }
+            });
+        }
+        return false;
+    });
+
+    function cleanTnanks(form){
+        $('.js-window').hide();
+        $("input[type=text]").val("");
+        $('a[href=#js-form2]').trigger('click');
+                // location = "thanks.php";
+            };
+
 
     $('.az-select').each(function(){
         var select = $(this);    
