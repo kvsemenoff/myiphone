1cd84932f3eb9f63a23cf3f13326cfbc4261ff4c
Berzeg07 <berezg86@gmail.com>
2017-04-04 17:17:34 +0400

    29.14

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,36 @@
 <section class="section section_padding">
-<div class=" container">
-	<div class="row">
-		<div class="col-md-12">
+	<div class=" container">
+
+		<div class="row">
+			<div class="col-md-6 padding_no">
+				<div class="dd-left">
+					<span>Выберите свой <br>подарок</span>
+				</div>
+				<!-- <img class="img-responsive" src="img/bl2.jpg" alt=""> -->
+			</div>
+			<div class="col-md-6 padding_no_2">
+				<div class="dd-right">
+
+					<div class="dd-form">
+
+						<form class="js-submit" action="#" method="post">
+							<div class="dd-inputs">
+								<input class="dd-name" type="text" name="uname" placeholder="Ваше имя">
+							</div>
+							<div class="dd-inputs">
+								<input class="phone dd-phone" type="text" name="phone" placeholder="Ваше телефон">
+							</div>
+							<div class="dd-inputs">
+								<input class="dd-but" type="submit" value="Заказать">
+							</div>
+						</form>
+
+					</div>
+
+				</div>
+			</div>
+			<div class="clearfix"></div>
 		</div>
+
 	</div>
-</div>
 </section>
